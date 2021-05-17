5cbba21cc2f43cd9ab16b62ed04dc098ae77bcb8
Oprincely <oprincely@gmail.com>
2021-05-17 11:55:16 +0100

    v1

--- app.py
+++ app.py
@@ -18,12 +18,3 @@ from models import Result
 @app.route('/')
 def hello():
     return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-
-if __name__ == '__main__':
-    app.run()
