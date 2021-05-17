a8711a5de2621fdf1d476cae98cdde251a3c271f
Oprincely <oprincely@gmail.com>
2021-05-17 11:06:36 +0100

    v1.0.0

--- config.py
+++ config.py
@@ -1,16 +1,29 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
+
+class Config(object):
     DEBUG = False
-    DEVELOPMENT = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:spywizard@localhost:5432/wordcount_dev'#os.environ['DATABASE_URL']
+    print("SQLALCHEMY_DATABASE_URI = ",SQLALCHEMY_DATABASE_URI)
+
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
+
 
 class StagingConfig(Config):
+    DEVELOPMENT = True
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
-    DEBUG = True
     DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
